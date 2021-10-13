6c72dae7c5adf4da334cf4174ca727e3d86415e5
someh <someh2705@gmail.com>
2021-10-13 23:34:57 +0900

    init: Start android project

---
+++ app/src/main/java/com/study/jetpack/MainActivity.kt
@@ -0,0 +1,38 @@
+git package com.study.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.study.jetpack.ui.theme.JetPackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackTheme {
+        Greeting("Android")
+    }
+}
